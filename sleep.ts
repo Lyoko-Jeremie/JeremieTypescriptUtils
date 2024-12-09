c313266bc8a5eca4ae7e819fdba24ddb62bd1df2
LyokoJeremie <lucheng989898@gmail.com>
2024-12-09 18:03:06 +0800

    .

--- sleep.ts
+++ sleep.ts
@@ -1,3 +1,10 @@
 export async function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function sleepWithCancel(ms: number): [PromiseWithResolvers<any>, ReturnType<typeof setTimeout>] {
+    let cancelHandle: ReturnType<typeof setTimeout>;
+    const p = Promise.withResolvers();
+    cancelHandle = setTimeout(p.resolve, ms);
+    return [p, cancelHandle];
+}
