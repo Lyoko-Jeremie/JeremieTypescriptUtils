2d6c598a408418e4ad867cf3daf45da4d48f5e3d
LyokoJeremie <lucheng989898@gmail.com>
2024-12-31 18:09:43 +0800

    Update CustomReadonlyMapHelper.ts

--- CustomReadonlyMapHelper.ts
+++ CustomReadonlyMapHelper.ts
@@ -1,3 +1,6 @@
+// import {CustomIterableIterator} from "./CustomIterableIterator";
+import {CustomMapIterator} from "./CustomMapIterator";
+
 export abstract class CustomReadonlyMapHelper<K, V> implements ReadonlyMap<K, V> {
 
     abstract get size(): number;
@@ -8,87 +11,40 @@ export abstract class CustomReadonlyMapHelper<K, V> implements ReadonlyMap<K, V>
 
     abstract entries(): ReturnType<ReadonlyMap<K, V>['entries']>;
 
-    [Symbol.iterator]() {
+    [Symbol.iterator](): ReturnType<ReadonlyMap<K, V>['entries']> {
         return this.entries();
     }
 
-    forEach(callback: (value: V, key: K, map: ReadonlyMap<K, V>) => void, thisArg?: any): void {
+    forEach(callback: (value: V, key: K, map: CustomReadonlyMapHelper<K, V>) => void, thisArg?: any): void {
         for (const nn of this.entries()) {
-            callback(nn[1], nn[0], this);
+            callback(this.get(nn[0])!, nn[0], this);
         }
     }
 
-    keys(): ReturnType<ReadonlyMap<K, V>['keys']> {
-        return this.entries().map(T => T[0]);
+    keys()/*: ReturnType<ReadonlyMap<K, V>['keys']>*/ {
+        return new CustomMapIterator<K, typeof this, [K, V][]>(
+            this,
+            (index, p, ito) => {
+                return {
+                    done: index >= this.size,
+                    value: ito.cache[index]?.[0],
+                };
+            },
+            Array.from(this.entries()),
+        );
     }
 
-    values(): ReturnType<ReadonlyMap<K, V>['values']> {
-        return this.entries().map(T => T[1]);
-    }
-
-}
-
-// ================ Example ================
-
-class A extends CustomReadonlyMapHelper<string, number> {
-    data: { k: string, v: number }[] = [];
-
-    entries(): ReturnType<ReadonlyMap<string, number>["entries"]> {
-        return this.data.entries().map((n) => [n[1].k, n[1].v]);
-    }
-
-    get(key: string): number | undefined {
-        return this.data.find(({k}) => k === key)?.v;
-    }
-
-    has(key: string): boolean {
-        return !!this.data.find(({k}) => k === key);
-    }
-
-    get size(): number {
-        return this.data.length;
-    }
-
-}
-
-class B extends CustomReadonlyMapHelper<string, number> {
-    data: Set<{ k: string, v: number }> = new Set();
-
-    entries(): ReturnType<ReadonlyMap<string, number>["entries"]> {
-        return this.data.entries().map((n) => [n[1].k, n[1].v]);
-    }
-
-    get(key: string): number | undefined {
-        return this.data.values().find(({k}) => k === key)?.v;
-    }
-
-    has(key: string): boolean {
-        return !!this.data.values().find(({k}) => k === key);
-    }
-
-    get size(): number {
-        return this.data.size;
-    }
-
-}
-
-class C extends CustomReadonlyMapHelper<string, number> {
-    data: Map<string, { k: string, v: number }> = new Map();
-
-    entries(): ReturnType<ReadonlyMap<string, number>["entries"]> {
-        return this.data.entries().map((n) => [n[1].k, n[1].v]);
-    }
-
-    get(key: string): number | undefined {
-        return this.data.get(key)?.v;
-    }
-
-    has(key: string): boolean {
-        return this.data.has(key);
-    }
-
-    get size(): number {
-        return this.data.size;
+    values()/*: ReturnType<ReadonlyMap<K, V>['values']>*/ {
+        return new CustomMapIterator<V, typeof this, [K, V][]>(
+            this,
+            (index, p, ito) => {
+                return {
+                    done: index >= this.size,
+                    value: ito.cache[index]?.[1],
+                };
+            },
+            Array.from(this.entries()),
+        );
     }
 
 }
